f437765d44b0923f852bf251c1dd5a5752a8415b
Christian Sachs <sachs.christian@gmail.com>
2024-04-10 00:05:59 +0200

    Version 7.0.0

--- pflake8/__init__.py
+++ pflake8/__init__.py
@@ -1,6 +1,6 @@
 """ pyproject-flake8 (`pflake8`), a monkey patching wrapper to connect flake8 with pyproject.toml configuration """  # noqa
 
-__version__ = '7.0.0a1'
+__version__ = '7.0.0'
 
 import ast
 import configparser
